14dbad2c03ec425e84e7ceaef5a8971b3078cb2f
Abubakar <59839663+Abubakar95@users.noreply.github.com>
2020-02-23 10:18:03 +0400

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default class about extends Component {
                     <div style={{
                         height: '70vh',
                         textAlign: 'center',
-                        fontSize: '4rem',
+                        fontSize: '1.5rem',
                         display: 'table',
                         width: '100%',
                         backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,.7)), url(${require('../assets/img/bg-masthead.jpg')})`
@@ -18,7 +18,11 @@ export default class about extends Component {
                             textAlign: 'center',
                             color: 'white'
                         }}>
-                            About Us
+                            We are an on campus sports enthusiasts community where fellow classmates <br></br>
+                            can create and organize games from variety of our sports. We help keep track of 
+                            all athletes attending, <br></br>so you don't have to worry about the lack of players 
+                            showing up. <br></br>Sign up now and create your very first sporting event!
+
                         </p>
                     </div>
                 </div>
